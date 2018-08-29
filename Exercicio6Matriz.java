19970ff37dea601725c1105529c5da6bd321198a
Adrelayne Souza <33074959+adrelayne@users.noreply.github.com>
2018-08-29 19:30:08 -0300

    Update Exercicio6Matriz.java

--- Exercicio6Matriz.java
+++ Exercicio6Matriz.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+public class ExerciciosMatriz {
+    public static void exibeNum10a20(int[][] matriz) {
+        /*  Elaborar o método exibeNum10a20, que recebe uma matriz de 
+        inteiros e exibe a quantidade de elementos existentes em cada linha,
+        entre 10 e 20.
+        Elaborar o método mediaPares, que recebe uma matriz de inteiros e 
+        devolve a média dos elementos pares existentes na matriz.
+        Elaborar o método main que solicita que o usuário digite o número de
+        linhas e colunas da matriz. Depois, solicita que o usuário digite os
+        valores dos elementos dessa matriz. Chame o método exibeNum10a20 e o
+        método mediaPares. Exiba a média dos elementos pares da matriz.
+        */
