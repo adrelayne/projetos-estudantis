4d67eea36591d1de2ddcbf43642847e30efa1c92
Adrelayne Souza <33074959+adrelayne@users.noreply.github.com>
2018-08-29 18:13:10 -0300

    Create Exercicio5Matriz.java

---
+++ Exercicio5Matriz.java
@@ -0,0 +1,15 @@
+package exercicios.matriz;
+
+public class ExerciciosMatriz {
+    public static void main(String[] args) {
+        /*  Elabore um programa em que leia os valores de dois vetores
+            de 4 elementos inteiros cada um. Crie uma matriz 4x2, sendo
+            que a primeira coluna dessa matriz deve corresponder ao 
+            primeiro vetor e a segunda coluna dessa matriz deve
+            corresponder ao segundo vetor. Exiba a matriz.        
+        */
+        
+        
+    }
+    
+}
