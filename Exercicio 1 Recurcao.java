ff398e6387777f5c3679cf488e8015eb7149560f
Adrelayne Souza <33074959+adrelayne@users.noreply.github.com>
2018-09-12 20:56:05 -0300

    Update Exercicio 1 Recurcao.java

--- Exercicio 1 Recurcao.java
+++ Exercicio 1 Recurcao.java
@@ -4,16 +4,25 @@ package exercicios.recursao;
         
 public class ExerciciosRecursao {
 
-    public static int soma(int n){
+    public static int somaRecursiva(int n){
         if(n >= 0){
-            int resultado = n + soma(n-1);
+            int resultado = n + somaRecursiva(n-1);
             return resultado;
         }
         else{
             return 0;
         }
     }
+    public static void somaIterativa(int n) {
+        int resultado = 0;
+        for (int i = 1; i <= n; i++){
+            resultado += i;
+            System.out.println(resultado);
+        }
+    }
+    
     public static void main(String[] args) {
-        System.out.println(soma(5));
+        System.out.println(somaRecursiva(5));
+        somaIterativa(5);
     }
 }
